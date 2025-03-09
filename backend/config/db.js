a22a49a5fd21c3c845c74dc772dcd14d42a61f72
it21156960 <chamith227@gmail.com>
2025-03-09 15:55:49 +0530

    db config updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,8 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1); // Exit with failure
